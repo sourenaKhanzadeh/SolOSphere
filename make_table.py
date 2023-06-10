29530cc6baa752972147dc888028e288f48a1874
Sourena Khanzadeh <sourena.khanzadeh@gmail.com>
2023-06-09 21:37:02 -0400

    Experiments updated

--- make_table.py
+++ make_table.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
 from ast import literal_eval
 import pprint as pp
 
@@ -15,42 +17,56 @@ def make_csv_to_dict(csv_name):
     return df.to_dict('list')
 
 
-def make_txt_to_list_of_dict(txt_name):
-    list_of_dict = []
+def make_txt_to_df(txt_name):
+    # read the data line by line
     with open(txt_name, 'r') as f:
-        for line in f:
-            line = line.strip()
-            if line != '':
-                # turn line into a dictionary
-                line = literal_eval(line)
-                # add line to dict_to_csv
-                list_of_dict.append(line)
-    return list_of_dict
+        lines = f.readlines()
 
+    # convert each line to a dictionary
+    data_dicts = [literal_eval(line) for line in lines]
 
-def list_of_dict_to_pandas(list_of_dict):
-    df = pd.DataFrame(list_of_dict)
+    # flatten dictionaries and convert to DataFrame
+    df = pd.json_normalize(data_dicts)
+
     return df
 
 
-def make_headers(list_of_dict):
-    headers = []
-    for dict in list_of_dict:
-        for key in dict.keys():
-            if key not in headers:
-                headers.append(key)
-    return headers
-
 FILE = "data/custom.txt"
-dict_to_csv = make_txt_to_list_of_dict(FILE)
-headers = make_headers(dict_to_csv)
-df = list_of_dict_to_pandas(dict_to_csv)
+df = make_txt_to_df(FILE)
+# dict_to_csv = make_txt_to_list_of_dict(FILE)
+# headers = make_headers(dict_to_csv)
+# df = list_of_dict_to_pandas(dict_to_csv)
 
-# add headers to dataframe
-df = df.reindex(columns=headers)
-# show all columns
-pd.set_option('display.max_columns', None)
+print(df.head())
 
-pp.pprint(df.head())
+# visualize data
+# Bar plot example
+plt.figure(figsize=(12,8))
+# sns.barplot(x='contract2.name', y='contract2.gas', data=df)
+# plt.xticks(rotation=90)  # Rotate x-axis labels for better visibility if they're long
+# plt.title('Contract1 Gas usage')
+# plt.show()
 
-df.to_latex(FILE.replace('.txt', '.tex'), index=False)
+# Plot contract1 loop24
+sns.barplot(x=df['contract1.name'], y=df['loop24.contract1'], color='blue', label="Contract 1 Loop24")
+
+# Plot contract2 loop24
+sns.barplot(x=df['contract2.name'], y=df['loop24.contract2'], color='green', label="Contract 2 Loop24")
+
+# Add a legend and informative axis label
+plt.legend(ncol=2, loc="upper right", frameon=True)
+plt.xlabel('Contracts', fontsize=15)
+plt.xticks(rotation=90)
+plt.ylabel('Loop24', fontsize=15)
+plt.title('Comparison of Loop24 between contracts', fontsize=20)
+
+plt.show()
+
+# # add headers to dataframe
+# df = df.reindex(columns=headers)
+# # show all columns
+# pd.set_option('display.max_columns', None)
+#
+# pp.pprint(df.head())
+#
+# df.to_latex(FILE.replace('.txt', '.tex'), index=False)
